d9c5b80d94a8045420ca43de0284fed039c89f36
Asia Hutchins <125086964+asiaaitch@users.noreply.github.com>
2023-04-20 22:17:21 -0400

    Add files via upload
    made changes to main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const rapperName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://fruit-of-the-spirit-api.onrender.com/api/${fruitName}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data.verseOne,
+        document.querySelector('#verseTwo').innerText = data.verseTwo,  
+        document.querySelector('#verseThree').innerText = data.verseThree
+    }catch(error){
+        console.log(error)
+    }
+}
